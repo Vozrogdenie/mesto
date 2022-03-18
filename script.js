72d14c9b294e28dfab992959eb722d5c004b0888
Vozrogdenie <93585141+Vozrogdenie@users.noreply.github.com>
2022-03-18 13:03:34 +0300

    input

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.popup-open');
 const closePopup = popup.querySelector('.popup__close')
 
+function togglePopup(){
+    popup.classList.toggle('popup__open');
+}
 
-openPopup.addEventListener('click', function() {
- popup.classList.add('.popup__open')
-})
+openPopup.addEventListener('click', togglePopup);
+closePopup.addEventListener('click', togglePopup);
 
-closePopup.addEventListener('click', function() {
-    popup.classList.remove('.popup__open')
-   })
+
+const popupName = document.querySelector('.popup__name')
+popupName.value = 'Жак-Ив-Кусто'
+
+const popupProfession = document.querySelector('.popup__profession')
+popupProfession.value = 'Иследователь океана'
